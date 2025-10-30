d528cc74380a86b488511cbb73813e1c2d50b1bc
IAmzColbz <colbyriddle22@gmail.com>
2025-10-30 14:59:20 -0500

    docker migration

--- src/config/config.go
+++ src/config/config.go
@@ -1,1 +1,45 @@
 package config
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+type Config struct {
+	DatabaseURL string
+	JWTSecret   string
+
+	dbHost     string
+	dbPort     string
+	dbUser     string
+	dbPassword string
+	dbName     string
+}
+
+func LoadConfig(path string) (*Config, error) {
+	_ = godotenv.Load(path)
+
+	cfg := &Config{
+		dbHost:     os.Getenv("DB_HOST"),
+		dbPort:     os.Getenv("DB_PORT"),
+		dbUser:     os.Getenv("POSTGRES_USER"),
+		dbPassword: os.Getenv("POSTGRESS_PASSWORD"),
+		dbName:     os.Getenv("POSTGRES_DB"),
+		JWTSecret:  os.Getenv("SECRET_KEY"),
+	}
+
+	if cfg.dbHost == "" || cfg.dbPort == "" || cfg.dbUser == "" || cfg.dbName == "" {
+		return nil, fmt.Errorf("err: one or more database env variables are missing")
+	}
+	if cfg.JWTSecret == "" {
+		return nil, fmt.Errorf("err: SECRET_KEY env variable is missing")
+	}
+
+	cfg.DatabaseURL = fmt.Sprintf("postgresql://%s:%s@%s:%s/%s",
+		cfg.dbUser, cfg.dbPassword, cfg.dbHost, cfg.dbPort, cfg.dbName,
+	)
+
+	return cfg, nil
+}
--- src/main.go
+++ src/main.go
@@ -2,16 +2,34 @@ package main
 
 import (
 	"log"
-	"net/http"
+	"os"
+
+	"cryptachat-server/config"
+	"cryptachat-server/http"
+	"cryptachat-server/store"
 )
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Hello from the new Go server!"))
-	})
+	// Load config
+	cfg, err := config.LoadConfig("../.config/docker.env")
+	if err != nil {
+		log.Fatalf("FATAL: could not load configuration file: %v", err)
+	}
 
-	log.Println("Go server starting on :5001")
-	if err := http.ListenAndServe(":5001", nil); err != nil {
-		log.Fatal(err)
+	// Init DB Store
+	dbStore, err := store.NewPostgresStore(cfg.DatabaseURL, "../server/schema.sql")
+	if err != nil {
+		log.Fatalf("FATAL: could not connect to database: %v", err)
+	}
+	log.Println("Database connection establishand and schema initialized.")
+
+	// Init http
+	server := http.NewServer(cfg, dbStore)
+	log.Println("HTTP server initialized.")
+
+	// Start server
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "5000"
 	}
 }
